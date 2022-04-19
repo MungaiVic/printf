564281f15c2233f70233bee2a7b9f6dab48de0d5
MungaiVic <victornmungai@gmail.com>
2022-04-19 15:36:38 +0300

    prinf-task1

--- main.h
+++ main.h
@@ -4,7 +4,24 @@
 #include <stdlib.h>
 #include <stdarg.h>
 
-int _printf(const char *format, ...);
 
+/**
+ * struct print - Checks what flags need to be turned on
+ * @t: the type to be printed
+ * @f: the function to print
+ */
+
+typedef struct print
+{
+	char *t;
+	int (*f)(va_list);
+} print_t;
+
+int _putchar(char c);
+int _printf(const char *format, ...);
+int print_i(va_list i);
+int print_d(va_list d);
+int print_c(va_list c);
+int print_s(va_list s);
 
 #endif
